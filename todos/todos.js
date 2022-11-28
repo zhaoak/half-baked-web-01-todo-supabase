4c9198c65dd4e7852b54f31fdf4d571bd8629b37
Allie Zhao <allie.k.zhao@gmail.com>
2022-11-28 11:11:37 -0800

    adding todo to db working, no display functionality yet

--- todos/todos.js
+++ todos/todos.js
@@ -1,10 +1,10 @@
-import { 
-    checkAuth, 
-    createTodo, 
+import {
+    checkAuth,
+    createTodo,
     completeTodo,
     getTodos,
     logout,
-    deleteAllTodos, 
+    deleteAllTodos,
 } from '../fetch-utils.js';
 import { renderTodo } from '../render-utils.js';
 
@@ -16,33 +16,35 @@ const logoutButton = document.querySelector('#logout');
 const deleteButton = document.querySelector('.delete-button');
 
 todoForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const formData = new FormData(todoForm);
+
     // on submit, create a todo, reset the form, and display the todos
+    const newTodo = { todo: formData.get('todo') };
+    createTodo(newTodo);
 });
 
 // create todo state
 
 // add async complete todo handler function
-    // call completeTodo
-    // swap out todo in array
-    // call displayTodos
-
-   
+// call completeTodo
+// swap out todo in array
+// call displayTodos
 
 async function displayTodos() {
     // clear the container (.innerHTML = '')
-    // display the list of todos, 
-          // call render function, pass in state and complete handler function!
-          // append to .todos
+    // display the list of todos,
+    // call render function, pass in state and complete handler function!
+    // append to .todos
 }
 
 // add page load function
-    // fetch the todos and store in state
-    // call displayTodos
+// fetch the todos and store in state
+// call displayTodos
 
 logoutButton.addEventListener('click', () => {
     logout();
 });
-
 
 deleteButton.addEventListener('click', async () => {
     // delete all todos
