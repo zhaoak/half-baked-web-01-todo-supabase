2e13c83e9f676c0a2e1861cb50a7aac05c17486e
Allie Zhao <allie.k.zhao@gmail.com>
2022-11-28 11:52:24 -0800

    new todos render and add to list when submitted now

--- todos/todos.js
+++ todos/todos.js
@@ -15,16 +15,17 @@ const todoForm = document.querySelector('.todo-form');
 const logoutButton = document.querySelector('#logout');
 const deleteButton = document.querySelector('.delete-button');
 
+// create todo state
+let todoList = [];
+
 todoForm.addEventListener('submit', async (e) => {
     e.preventDefault();
     const formData = new FormData(todoForm);
 
     // on submit, create a todo, reset the form, and display the todos
     createTodo(formData.get('todo'));
+    await displayTodos();
 });
-
-// create todo state
-let todoList = [];
 
 // add async complete todo handler function
 // call completeTodo
@@ -32,10 +33,10 @@ let todoList = [];
 // call displayTodos
 
 async function displayTodos() {
+    todoList = await getTodos();
     // clear the container (.innerHTML = '')
     todosEl.innerHTML = '';
     // display the list of todos,
-    console.log(todoList);
     for (let todo of todoList) {
         // call render function, pass in state and complete handler function!
         // append to .todos
@@ -48,7 +49,7 @@ window.addEventListener('load', async () => {
     // fetch the todos and store in state
     todoList = await getTodos();
     // call displayTodos
-    displayTodos();
+    await displayTodos();
 });
 
 logoutButton.addEventListener('click', () => {
